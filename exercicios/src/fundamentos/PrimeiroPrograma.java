55f74b09b85d6c7a60bc00168648ae34acf45017
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-06-11 14:51:18 -0300

    19. comentarios

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,28 @@
 package fundamentos;
+/**
+ * Essa classe representa...
+ * @author eduardo
+ * @version 1.0.0
+ */
 
 public class PrimeiroPrograma {
 
+	// /** + enter cria um comentario javadoc
+	/**
+	 * 
+	 * @param args
+	 */
+	
 	public static void main(String[] args) {
+		// Uma sentença de codigo termina com ;
+		// Outro comentario
+		// comentario
 		System.out.println("Primeiro Programa Parte #01!");
+		// System.out.println("Primeiro Programa Parte #01!");
+		
+		/*
+		 * Linha 1 Linha 2
+		 */
 		System.out.println("Primeiro Programa Parte #02!");
 	}
 
