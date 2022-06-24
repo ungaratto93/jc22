3eb6c1f2765ccedf31907506f2f3a8119cd1ab94
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-06-23 21:20:17 -0300

    31. Objeto vs Primitivo

---
+++ exercicios/src/fundamentos/PrimitivoVsObjeto.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class PrimitivoVsObjeto {
+	public static void main(String[] args) {
+		
+		String s = new String("texto");
+		s.toUpperCase();
+		
+		// wrappers, são a versão objeto dos tipos primitivos 
+		int a = 123;
+		
+		
+	}
+}
